7a2a9d12af955b5957cf47ceaf84df847912c994
Graham Ullrich <graham@flyingcracker.com>
2016-08-12 11:45:34 -0600

    flake8 fix

--- pinax/identity/__init__.py
+++ pinax/identity/__init__.py
@@ -4,5 +4,5 @@ import pkg_resources
 __version__ = pkg_resources.get_distribution("pinax-identity").version
 
 
-from .authentication import TokenAuthentication  #noqa
+from .authentication import TokenAuthentication  # noqa
 from .permissions import ensure_scope  # noqa
