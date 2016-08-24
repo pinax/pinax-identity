d7f53e728c157eaa7b44092470341b65bf135ad8
Brian Rosner <brian@brosner.com>
2016-08-23 20:45:21 -0600

    removed imports causing AppRegistryNotReady

--- pinax/identity/__init__.py
+++ pinax/identity/__init__.py
@@ -2,7 +2,3 @@ import pkg_resources
 
 
 __version__ = pkg_resources.get_distribution("pinax-identity").version
-
-
-from .authentication import TokenAuthentication  # noqa
-from .permissions import ensure_scope  # noqa
